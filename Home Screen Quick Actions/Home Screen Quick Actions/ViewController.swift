7fc509f33a90102768f9f693ceba6f2b56d3f143
马卿 <admin@maiqngdemac-mini.local>
2016-09-30 16:27:39 +0800

    UIKit Peek and Pop 完善中

--- Home Screen Quick Actions/Home Screen Quick Actions/ViewController.swift
+++ Home Screen Quick Actions/Home Screen Quick Actions/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    struct PreviewDetail {
+        let title: String
+        let preferredHeight: Double
+    }
+    var tableView : UITableView?
+    let sampleData = [
+        PreviewDetail(title: "Small", preferredHeight: 160.0),
+        PreviewDetail(title: "Medium", preferredHeight: 320.0),
+        PreviewDetail(title: "Large", preferredHeight: 0.0) // 0.0 to get the default height.
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView = UITableView(frame: view.bounds)
+        tableView?.registerClass(UITableViewCell().classForCoder, forCellReuseIdentifier: "cell")
+        tableView?.delegate = self
+        //Check for force touch feature, and add force touch/previewing capability.
+        if traitCollection.forceTouchCapability == .Available{
+            /*
+             Register for `UIViewControllerPreviewingDelegate` to enable
+             "Peek" and "Pop".
+             (see: MasterViewController+UIViewControllerPreviewing.swift)
+             
+             The view controller will be automatically unregistered when it is
+             deallocated.
+             */
+            
+            registerForPreviewingWithDelegate(self, sourceView: tableView!)
+        } else {
+            // Create an alert to display to the user.
+          
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
+extension ViewController : UITableViewDataSource,UITableViewDelegate{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
+extension ViewController : UIViewControllerPreviewingDelegate{
+    
+    func previewingContext(previewingContext: UIViewControllerPreviewing, commitViewController viewControllerToCommit: UIViewController) {
+        
+    }
+    func previewingContext(previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        return UIViewController()
+    }
+}
